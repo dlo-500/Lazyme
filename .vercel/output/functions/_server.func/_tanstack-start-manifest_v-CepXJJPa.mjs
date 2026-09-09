8bffec6f92c5776c9da2b0f2f22bed40890fc66a
dlo-500 <lonetariq500@gmail.com>
2026-09-09 21:20:08 +0530

    Add files via upload

---
+++ .vercel/output/functions/_server.func/_tanstack-start-manifest_v-CepXJJPa.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-CepXJJPa.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/workspace/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-CCk6vPAM.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-CCk6vPAM.js"
+		} }]
+	},
+	"/": {
+		filePath: "/workspace/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-BI5dhIR1.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
